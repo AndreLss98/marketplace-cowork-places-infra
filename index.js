f6fd887aaf9bae27ab5be96f483af9718209d2a9
AndreLss <andre_luis98@outlook.com>
2020-05-05 18:23:36 -0300

    Add dependencia do bcrypt body-parser e dotenv ao projeto

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 require('dotenv').config();
 const express = require("express");
 const app = express();
+const boydParser = require('body-parser');
 const app_port = process.env.PORT || 3000;
+
+
+app.use(boydParser.json());
+app.use(boydParser.urlencoded({extended: false}));
+
+require('./controllers/usuario')(app);
 
 app.listen(app_port, () => console.log(`Server running on port ${app_port}`));
