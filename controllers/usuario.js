b3a3f021760f05e9af19cb8d6531d0e679f5b405
AndreLss <andre_luis98@outlook.com>
2020-05-05 20:44:33 -0300

    Cria endpoint para autenticar usuario

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -1,9 +1,11 @@
+const bcrypt = require('bcryptjs');
 const router = require('express').Router();
 const mdPagination = require('./../middlewares/pagination');
 
 const Usuario = require('../repositorys/usuario');
 
 router.get('/', mdPagination(Usuario.getAll), async (req, res, next) => {
+    res.result.results.forEach(user => user.senha = undefined);
     res.send(res.result);
 });
 
@@ -29,6 +31,19 @@ router.post('/create', async (req, res, next) => {
     } catch (err) {
         return res.status(400).send({ error: "Registrarion Failed!" });
     }
+});
+
+router.post('/authenticate', async (req, res) => {
+    const { email, senha } = req.body;
+    const user = await Usuario.getByEmail(email);
+
+    if (!user) return res.status(404).send({ error: "User not found" });
+
+    if (!await bcrypt.compare(senha, user.senha)) return res.status(400).send({ error: "Invalid password" });
+
+    user.senha = undefined;
+
+    res.status(200).send(user);
 });
 
 module.exports = app => app.use('/usuarios', router);
