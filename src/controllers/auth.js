1bb3d524145135f55e4642434828f81d4a821425
AndreLss <andre_luis98@outlook.com>
2020-05-06 00:16:48 -0300

    Cria controller dedicado para autenticacao

---
+++ src/controllers/auth.js
@@ -0,0 +1,24 @@
+const bcrypt = require('bcryptjs');
+const router = require('express').Router();
+
+const shared = require('./../shared/functions');
+const Usuario = require('../repositorys/usuario');
+
+router.post('/login', async (req, res) => {
+    const { email, senha } = req.body;
+    const user = await Usuario.getByEmail(email);
+
+    if (!user) return res.status(404).send({ error: "User not found" });
+
+    if (!await bcrypt.compare(senha, user.senha)) return res.status(400).send({ error: "Invalid password" });
+
+    user.senha = undefined;
+
+    res.status(200).send({ user, token: shared.generateToken({ id: user.id }) });
+});
+
+router.post('/google', async (req, res, next)) {
+
+}
+
+module.exports = app => app.use('/auth', router);
--- src/controllers/usuario.js
+++ src/controllers/usuario.js
@@ -1,17 +1,10 @@
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 const router = require('express').Router();
 
 const authMiddleware = require('../middlewares/auth');
 const paginationMiddleware = require('../middlewares/pagination');
 
+const shared = require('./../shared/functions');
 const Usuario = require('../repositorys/usuario');
-
-function generateToken(params = {}) {
-    return jwt.sign(params, process.env.JWT_SECRET, {
-        expiresIn: 60
-    });
-}
 
 router.get('/', authMiddleware, paginationMiddleware(Usuario.getAll), async (req, res, next) => {
     res.result.results.forEach(user => user.senha = undefined);
@@ -36,23 +29,10 @@ router.post('/create', async (req, res, next) => {
     try {
         const user = await Usuario.save(req.body);
         user.senha = undefined;
-        return res.status(200).send({ user, token: generateToken({ id: user.id }) });
+        return res.status(200).send({ user, token: shared.generateToken({ id: user.id }) });
     } catch (err) {
         return res.status(400).send({ error: "Registrarion Failed!" });
     }
-});
-
-router.post('/authenticate', async (req, res) => {
-    const { email, senha } = req.body;
-    const user = await Usuario.getByEmail(email);
-
-    if (!user) return res.status(404).send({ error: "User not found" });
-
-    if (!await bcrypt.compare(senha, user.senha)) return res.status(400).send({ error: "Invalid password" });
-
-    user.senha = undefined;
-
-    res.status(200).send({ user, token: generateToken({ id: user.id }) });
 });
 
 module.exports = app => app.use('/usuarios', router);
