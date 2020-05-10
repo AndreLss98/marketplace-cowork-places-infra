fb559ee2c3f272c3b3cb722bc1a133a1f0eac05a
AndreLss <andre_luis98@outlook.com>
2020-05-10 18:44:56 -0300

    Valida a existencia de caracteristica no alugavel

--- src/repositorys/alugavel.js
+++ src/repositorys/alugavel.js
@@ -12,9 +12,11 @@ module.exports = {
     async save(alugavel, caracteristicas) {
         try {
             const id = await db(TABLE).insert(alugavel).returning('id');
-            caracteristicas.forEach(async (caracteristica) => {
-                await AlugavelCaracteristica.realacionar(id[0], caracteristica.id, caracteristica.valor);
-            });
+            if (caracteristicas) {
+                caracteristicas.forEach(async (caracteristica) => {
+                    await AlugavelCaracteristica.realacionar(id[0], caracteristica.id, caracteristica.valor);
+                });
+            }
             return await db(TABLE).where({ id: id[0] }).first();
         } catch(error) {
             throw error;
