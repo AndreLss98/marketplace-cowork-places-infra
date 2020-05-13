45d57dec831b68ac31b24b04c875f2b028a61618
AndreLss <andre_luis98@outlook.com>
2020-05-12 23:15:46 -0300

    Implementa endpoint para salvar aluguel

--- src/controllers/aluguel.js
+++ src/controllers/aluguel.js
@@ -6,10 +6,27 @@ const Aluguel = require('./../repositorys/aluguel');
 const DiasReservados = require('./../repositorys/dias_reservados');
 
 router.post('/', async (req, res, next) => {
-    const tempAluguel = req.body;
+    let tempAluguel = req.body;
     
+    const { dias_reservados } = tempAluguel;
+    delete tempAluguel.dias_reservados;
 
-    res.status(200).send({ response: 'ok' });
+    const aluguel = await Aluguel.save(tempAluguel);
+
+    for(let dia of dias_reservados) {
+        try {
+            dia.aluguel_id = aluguel.id;
+            dia.alugavel_id = aluguel.alugavel_id;
+            await DiasReservados.save(dia);
+        } catch(error) {
+            return res.status(400).send({ error: "Failed to reserve" });
+        }
+    }
+
+    delete aluguel.authorization_id;
+    delete aluguel.cancellation_id;
+
+    res.status(200).send(aluguel);
 });
 
 module.exports = app => app.use('/alugueis', authMiddleware, router);
