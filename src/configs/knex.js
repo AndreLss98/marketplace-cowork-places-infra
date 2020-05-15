606982898e35e3d99ee84540c8ffdd024b53ab33
AndreLss <andre_luis98@outlook.com>
2020-05-15 15:21:47 -0300

    Muda projeto para variaveis unicas de ambiente

--- src/configs/knex.js
+++ src/configs/knex.js
@@ -1,26 +1,14 @@
-const {
-    DB_TEST, DB_USER_TEST, DB_PASSWORD_TEST,
-    DB_PROD, DB_USER_PROD, DB_PASSWORD_PROD
-} = process.env;
+const { DB, DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
-CONFIGS = {
-    dev: {
-        client: 'pg',
-        connection: {
-            database: DB_TEST,
-            user: DB_USER_TEST,
-            password: DB_PASSWORD_TEST
-        }
-    },
-    prod: {
-        client: 'pg',
-        connection: {
-            database: DB_PROD,
-            user: DB_USER_PROD,
-            password: DB_PASSWORD_PROD
-        }
+const CONFIG = {
+    client: 'pg',
+    connection: {
+        host: DB_HOST,
+        database: DB,
+        user: DB_USER,
+        password: DB_PASSWORD
     }
 }
 
-const knex = require('knex')(CONFIGS[process.env.ENVIRONMENT]);
+const knex = require('knex')(CONFIG);
 module.exports = knex;
