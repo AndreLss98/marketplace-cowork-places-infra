91060640d519cd25627cc6506b92ea0b6ca75bf5
AndreLss <andre_luis98@outlook.com>
2020-05-23 16:25:03 -0300

    Corrige erro de funcao async na autenticacao do google

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -30,7 +30,7 @@ router.get('/google', passport.authenticate('google', {
     scope: ['profile', 'email']
 }));
 
-router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
+router.get('/google/redirect', passport.authenticate('google'), async (req, res) => {
 
     const refresh_token = shared.generateRefreshToken();
     await Usuario.update(req.user.id, { refresh_token });
