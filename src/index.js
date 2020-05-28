11e98f82f6e3ffaceb09ee594857582dad3c0950
AndreLss <andre_luis98@outlook.com>
2020-05-28 12:32:54 -0300

    Aceitando credenciais na requisicao

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ const boydParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const app_port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+    credentials: true
+}));
 app.use(cookieParser());
 app.use(boydParser.json());
 app.use(boydParser.urlencoded({ extended: true }));
