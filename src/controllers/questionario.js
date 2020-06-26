af5b76f24a09a3139748b750fcb1088d3ff637e7
AndreLss <andre_luis98@outlook.com>
2020-06-26 14:45:32 -0300

    Cria rota para deletar uma pergunta do questionario

--- src/controllers/questionario.js
+++ src/controllers/questionario.js
@@ -2,11 +2,15 @@ const router = require('express').Router();
 
 const Questionario = require('../repositorys/questionario');
 
+const authMiddleware = require('./../middlewares/auth');
+
+const perfis = require('./../shared/perfis');
+
 router.get('/', async (req, res, next) => {
     return res.status(200).send(await Questionario.getAll());
 });
 
-router.post('/', async (req, res, next) => {
+router.post('/', authMiddleware([perfis.ADMIN]), async (req, res, next) => {
     const { pergunta } = req.body;
     if (!pergunta) return res.status(400).send({ error: 'Question is required' });
 
@@ -18,4 +22,27 @@ router.post('/', async (req, res, next) => {
     }
 });
 
-module.exports = app => app.use('/questionario', router);
+router.put('/:id', authMiddleware([perfis.ADMIN]), async (req, res, next) => {
+    const { id } = req.params;
+    const { pergunta } = req.body;
+    if (!pergunta) return res.status(400).send({ error: 'Question is required' });
+
+    try {
+        const reponse = await Questionario.update(id, pergunta);
+        return res.status(200).send(reponse);
+    } catch (error) {
+        return res.status(400).send({ error: "Register failed" });
+    }
+});
+
+router.delete('/:id', authMiddleware([perfis.ADMIN]), async (req, res, next) => {
+    const { id } = req.params;
+    try {
+        const reponse = await Questionario.delete(id);
+        return res.status(200).send({ reponse });
+    } catch (error) {
+        return res.status(400).send({ error })
+    }
+});
+
+module.exports = app => app.use('/questionario', authMiddleware(), router);
--- src/repositorys/questionario.js
+++ src/repositorys/questionario.js
@@ -5,12 +5,28 @@ const ANSWERS_TABLE = 'questionario_usuario';
 
 module.exports = {
     async getAll() {
-        return await db(TABLE);
+        return await db(TABLE).orderBy('id', 'asc');
     },
     async save(pergunta) {
         try {
             const id = await db(TABLE).insert(pergunta).returning('id');
             return await db(TABLE).where({ id: id[0] }).first();
+        } catch(error) {
+            throw error;
+        }
+    },
+    async update(id, pergunta) {
+        try {
+            await db(TABLE).update({ pergunta }).where({ id });
+            return await db(TABLE).where({ id }).first();
+        } catch(error) {
+            throw error;
+        }
+    },
+    async delete(id) {
+        try {
+            await db(ANSWERS_TABLE).delete().where({ pergunta_id: id });
+            return await db(TABLE).delete().where({ id });
         } catch(error) {
             throw error;
         }
