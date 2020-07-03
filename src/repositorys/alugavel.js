f2c2eadca115698d3a21b62e28e3b4364bdb85ae
AndreLss <andre_luis98@outlook.com>
2020-07-03 15:43:11 -0300

    Retorna nota de um alugavel

--- src/repositorys/alugavel.js
+++ src/repositorys/alugavel.js
@@ -4,26 +4,37 @@ const TABLE = 'alugavel';
 const Info = require('./info');
 const Tipo = require('./tipo');
 const Local = require('./local');
+const Aluguel = require('./aluguel');
 const Caracteristica = require('./caracteristica');
 const AlugavelImagem = require('./alugavel_imagem');
-const AlugavelCaracteristica = require('./alugavel_caracteristica');
 const Documentos = require('./documentos_alugavel');
+const AlugavelCaracteristica = require('./alugavel_caracteristica');
 
 module.exports = {
     async getAll(filters = {}) {
         let alugaveis = await db(TABLE).where(filters);
         for (let alugavel of alugaveis) {
             alugavel.caracteristicas = [];
+
+            alugavel.infos = await Info.getAll(alugavel.id);
             alugavel.tipo = await Tipo.getById(alugavel.tipo_id);
-            alugavel.imagens = await AlugavelImagem.getAllByAlugavelId(alugavel.id);
             alugavel.local = await Local.getByAlugavelId(alugavel.id);
             alugavel.documentos = await Documentos.getAllByAlugavelId(alugavel.id);
-            alugavel.infos = await Info.getAll(alugavel.id);
+            alugavel.imagens = await AlugavelImagem.getAllByAlugavelId(alugavel.id);
+
+            const alugueis = await Aluguel.getAllByAlugavelId(alugavel.id);
+
+            if (alugueis && alugueis.length > 6) {
+                alugavel.nota = alugueis.reduce((soma, aluguel) => soma += aluguel.nota, 0) / alugueis.length;
+            }
+
             for (let info of alugavel.infos) {
                 delete info.alugavel_id;
             }
+
             delete alugavel.tipo_id;
             let tempCaracteristicas = await AlugavelCaracteristica.getAllCaracteristicas(alugavel.id);
+            
             for (let tempCaracteristica of tempCaracteristicas) {
                 let caracteristica = await Caracteristica.getById(tempCaracteristica.caracteristica_id);
                 caracteristica.valor = tempCaracteristica.valor;
@@ -35,23 +46,33 @@ module.exports = {
     },
     async getById(id) {
         let alugavel = await db(TABLE).where({ id }).first();
+        if (!alugavel) return alugavel;
         alugavel.caracteristicas = [];
-        alugavel.imagens = await AlugavelImagem.getAllByAlugavelId(id);
+
+        alugavel.infos = await Info.getAll(alugavel.id);
         alugavel.tipo = await Tipo.getById(alugavel.tipo_id);
         alugavel.local = await Local.getByAlugavelId(alugavel.id);
         alugavel.documentos = await Documentos.getAllByAlugavelId(alugavel.id);
-        alugavel.infos = await Info.getAll(alugavel.id);
+        alugavel.imagens = await AlugavelImagem.getAllByAlugavelId(alugavel.id);
+
+        const alugueis = await Aluguel.getAllByAlugavelId(alugavel.id);
+
+        if (alugueis && alugueis.length > 6) {
+            alugavel.nota = alugueis.reduce((soma, aluguel) => soma += aluguel.nota, 0) / alugueis.length;
+        }
+
         for (let info of alugavel.infos) {
             delete info.alugavel_id;
         }
+
         delete alugavel.tipo_id;
-        let tempCaracteristicas = await AlugavelCaracteristica.getAllCaracteristicas(id);
+        let tempCaracteristicas = await AlugavelCaracteristica.getAllCaracteristicas(alugavel.id);
+
         for (let tempCaracteristica of tempCaracteristicas) {
             let caracteristica = await Caracteristica.getById(tempCaracteristica.caracteristica_id);
             caracteristica.valor = tempCaracteristica.valor;
             alugavel.caracteristicas.push(caracteristica);
         }
-
         return alugavel;
     },
     async save(alugavel, caracteristicas, infos, local) {
@@ -73,7 +94,7 @@ module.exports = {
             await Local.save(local, id[0]);
 
             return await db(TABLE).where({ id: id[0] }).first();
-        } catch(error) {
+        } catch (error) {
             throw error;
         }
     },
