26c9b848f6d1320cec73fc1d47e40740bdf6cc2e
AndreLss <andre_luis98@outlook.com>
2020-07-11 12:03:17 -0300

    Cria endpoint para retornar nomes dos bairros

--- src/controllers/alugavel.js
+++ src/controllers/alugavel.js
@@ -57,6 +57,15 @@ router.get('/:id', async (req, res, next) => {
 });
 
 /**
+ * Retorna todos bairros cadastrados
+ */
+router.get('/local/bairros', async (req, res, next) => {
+    let filters = req.query.filters? JSON.parse(req.query.filters) : {};
+    const response = await Local.getAllBairros(filters);
+    return res.status(200).send(response);
+});
+
+/**
  * Retorna todas as informacoes de um alugavel 
  */
 router.get('/:id/infos', async (req, res, next) => {
--- src/repositorys/local.js
+++ src/repositorys/local.js
@@ -1,6 +1,8 @@
 const db = require('./../configs/knex');
 const TABLE = 'local';
 
+const { ALUGAVEL_STATUS } = require('./../shared/constants');
+
 module.exports = {
     async getByAlugavelId(alugavel_id) {
         return await db(TABLE).where({ alugavel_id }).first();
@@ -11,5 +13,11 @@ module.exports = {
     },
     async update(local) {
         return await db(TABLE).update(local).where({ alugavel_id: local.alugavel_id });
+    },
+    async getAllBairros(filters = {}) {
+        if (filters.used) return await db.select('bairro').from(TABLE).whereIn('alugavel_id', function() {
+            this.select('id').from('alugavel').where({ status: ALUGAVEL_STATUS.APPROVED });
+        }).groupBy('bairro').orderBy('bairro', 'asc');
+        return await db.select('bairro').from(TABLE).groupBy('bairro').orderBy('bairro', 'asc');
     }
 }
