533720c8d3bba0a47ec20dc791661ad4d6c9dafc
AndreLss <andre_luis98@outlook.com>
2020-08-01 12:06:46 -0300

    Define secure true for cookie config for prod env

--- src/controllers/usuario.js
+++ src/controllers/usuario.js
@@ -155,7 +155,7 @@ router.post('/create', async (req, res, next) => {
         delete user.email_token;
         
         return res
-        .cookie('refresh_token', refresh_token, { maxAge: expires_at, httpOnly: true, sameSite: 'none', secure: false })
+        .cookie('refresh_token', refresh_token, { maxAge: expires_at, httpOnly: true, sameSite: 'none', secure: true })
         .status(200)
         .send({ user, token: shared.generateToken({ id: user.id }), expires_at });
     } catch (err) {
