efcfb27e84c3da6bafbb27c10c39f46cb5fe6701
AndreLss <andre_luis98@outlook.com>
2020-08-03 09:19:17 -0300

    Melhora query de busca de dias ja reservados

--- src/repositorys/dias_reservados.js
+++ src/repositorys/dias_reservados.js
@@ -3,7 +3,12 @@ const TABLE = 'dias_reservados';
 
 module.exports = {
     async getAllByAlugavelId(alugavel_id) {
-        return await db(TABLE).where({ alugavel_id });
+        return await db.select(`${TABLE}.*`).from(TABLE)
+        .leftJoin('aluguel', `${TABLE}.aluguel_id`, 'aluguel.id')
+        .whereNull(`${TABLE}.aluguel_id`)
+        .orWhereNotNull('aluguel.paypal_order_id')
+        .orWhereNotNull('aluguel.subscription_id')
+        .andWhere(`${TABLE}.alugavel_id`, alugavel_id);
     },
     async save(dia) {
         try {
