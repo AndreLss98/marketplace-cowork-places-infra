953d98760983a705230fd34e92236ee7b2f3de3e
pedro <pedroramosdiehl@gmail.com>
2020-08-14 16:31:46 -0300

    Retorna dados do dono e endereço

--- src/repositorys/aluguel.js
+++ src/repositorys/aluguel.js
@@ -4,7 +4,16 @@ const TABLE = 'aluguel';
 
 module.exports = {
     async getAllByUsuarioId(usuario_id) {
-        return await db(TABLE).innerJoin('dias_reservados', 'dias_reservados.aluguel_id', `${TABLE}.id`).where(`${TABLE}.usuario_id`, usuario_id);
+        return await db(TABLE)
+            .select(`${TABLE}.*`)
+            .select('local.*')
+            .select('dias_reservados.*')
+            .select('usuario.nome as owner_nome', 'usuario.email as owner_email',  'usuario.numero_1 as owner_numero_1', 'usuario.numero_2 as owner_numero_2')
+            .innerJoin('dias_reservados', 'dias_reservados.aluguel_id', `${TABLE}.id`).where(`${TABLE}.usuario_id`, usuario_id)
+            .innerJoin('local', 'local.alugavel_id', `${TABLE}.alugavel_id`)
+            .innerJoin('usuario', 'usuario.id', usuario_id)
+
+
     },
     async getAllByAlugavelId(alugavel_id) {
         return await db(TABLE).where({ alugavel_id });
