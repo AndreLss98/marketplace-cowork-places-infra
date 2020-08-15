825fd10439cbf3ad919f2f77ba396b2a3d01ea3b
AndreLss <andre_luis98@outlook.com>
2020-08-15 12:35:53 -0300

    Melhora query para buscar dias reservados com alugues ativos

--- src/repositorys/dias_reservados.js
+++ src/repositorys/dias_reservados.js
@@ -3,12 +3,12 @@ const TABLE = 'dias_reservados';
 
 module.exports = {
     async getAllByAlugavelId(alugavel_id) {
-        return await db.select(`${TABLE}.*`).from(TABLE)
+        return db.select(`${TABLE}.*`).from(`${TABLE}`)
         .leftJoin('aluguel', `${TABLE}.aluguel_id`, 'aluguel.id')
-        .whereNull(`${TABLE}.aluguel_id`)
-        .orWhereNotNull('aluguel.paypal_order_id')
+        .where(`${TABLE}.alugavel_id`, alugavel_id)
+        .whereNotNull('aluguel.paypal_order_id')
         .orWhereNotNull('aluguel.subscription_id')
-        .andWhere(`${TABLE}.alugavel_id`, alugavel_id);
+        .orWhereNull(`${TABLE}.aluguel_id`);
     },
     async save(dia) {
         try {
