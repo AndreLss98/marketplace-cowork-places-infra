0dff835c5f513bfcf327445bf989183255d28bee
AndreLss <andre_luis98@outlook.com>
2021-01-14 20:13:21 -0300

    cria migration da tabela de cadastro de terceiro

---
+++ src/configs/migrations/20210114210714_create_table_cadastro_terceiro.js
@@ -0,0 +1,10 @@
+exports.up = knex => knex.schema.createTable('cadastro_terceiro', table => {
+    table.increments('id').notNullable().primary();
+    table.string('cnpj', 20);
+    table.string('cpf', 16);
+    table.string('nome', 250);
+    table.string('razao_social', 250);
+    table.integer('alugavel_id').notNullable().references('id').inTable('alugaveis');
+});
+
+exports.down = knex => knex.schema.dropTable('cadastro_terceiro');
