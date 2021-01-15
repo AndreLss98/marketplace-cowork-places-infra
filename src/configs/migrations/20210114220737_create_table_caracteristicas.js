661c025c8289a956ab5ca70ab97e8be013f0aeeb
AndreLss <andre_luis98@outlook.com>
2021-01-14 21:15:39 -0300

    cria migration da tabela de caracteristicas

---
+++ src/configs/migrations/20210114220737_create_table_caracteristicas.js
@@ -0,0 +1,9 @@
+exports.up = knex => knex.schema.createTable('caracteristica', table => {
+    table.increments('id').notNullable().primary();
+    table.integer('tipo_campo_id').notNullable().references('id').inTable('tipo_campo');
+    table.string('nome', 100).notNullable().unique();
+    table.string('icone', 100);
+    table.string('unidade_medida', 100);
+});
+
+exports.down = knex => knex.dropTable('caracteristica');
